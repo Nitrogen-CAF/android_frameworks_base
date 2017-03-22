1c6c244827379e92babb748412dcbe55fb708642
xyyx <xyyx@mail.ru>
2017-03-22 19:04:56 +0800

    MTP Dirty Hack (use mtp as default after connect to usb) (1/3) In device tree check this string:
    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
        persist.sys.usb.config=mtp

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3580,6 +3580,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to use the MTP by default after connecting to PC
+         * @hide
+         */
+        public static final String MTP_DIRTY_HACK = "mtp_dirty_hack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
